74854d8a2e51771178435a72bdbfdea280e32709
Sikander Singh <sikander441@localhost.localdomain>
2017-12-19 16:51:47 +0530

    problem code:FLOW001 status:ACCEPTED

---
+++ FLOW001.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ ios_base::sync_with_stdio(false);
+ long long int t;
+ cin>>t;
+ while(t--)
+ {
+  long long int a,b;
+  cin>>a>>b;
+  cout<<a+b<<endl;
+ }
+ return 0;
+}
